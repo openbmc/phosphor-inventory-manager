704b08df20ce13456ed7a23222989de59f96a1b6
Brad Bishop <bradleyb@fuzziesquirrel.com>
2016-10-19 21:19:04 -0400

    filters: Add propertiesChangedTo
    Add a signal match filter for properties changing to a specific
    value.
    
    Change-Id: I10f20ba03ae7c629d2c338c7975e0d32d9008e01
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- filters.hpp
+++ filters.hpp
@@ -106,12 +106,79 @@ struct Wrapper
     std::shared_ptr<holder::Base> _ptr;
 };
 
+namespace property_condition
+{
+
+/** @struct PropertyCondition
+ *  @brief Match filter functor that tests a property value.
+ *
+ *  @tparam T - The type of the property being tested.
+ *  @tparam U - The type of the condition checking functor.
+ */
+template <typename T, typename U>
+struct PropertyCondition
+{
+    PropertyCondition() = delete;
+    ~PropertyCondition() = default;
+    PropertyCondition(const PropertyCondition&) = default;
+    PropertyCondition & operator=(const PropertyCondition&) = delete;
+    PropertyCondition(PropertyCondition&&) = default;
+    PropertyCondition& operator=(PropertyCondition&&) = delete;
+    PropertyCondition(const char *iface, const char *property, U &&condition) :
+        _iface(iface),
+        _property(property),
+        _condition(std::forward<U>(condition)) { }
+
+    /** @brief Test a property value.
+     *
+     * Extract the property from the PropertiesChanged
+     * message and run the condition test.
+     */
+    bool operator()(sdbusplus::message::message &msg) const
+    {
+        std::map<
+            std::string,
+            sdbusplus::message::variant<T>> properties;
+        const char *iface = nullptr;
+
+        msg.read(iface);
+        if(strcmp(iface, _iface))
+                return false;
+
+        msg.read(properties);
+        auto it = properties.find(_property);
+        if(it == properties.cend())
+            return false;
+
+        return _condition(it->second);
+    }
+
+    private:
+    const char *_iface;
+    const char *_property;
+    U _condition;
+};
+
+} // namespace property_condition
 } // namespace details
 
 /** @brief The default filter.  */
 inline bool none(sdbusplus::message::message &) noexcept
 {
     return true;
+}
+
+/** @brief Implicit type deduction for constructing PropertyCondition.  */
+template <typename T>
+auto propertyChangedTo(
+        const char *iface,
+        const char *property,
+        T val)
+{
+    auto condition = [val](auto arg){return arg == val;};
+    using U = decltype(condition);
+    return details::property_condition::PropertyCondition<T, U>(
+            iface, property, std::move(condition));
 }
 
 } // namespace filters
