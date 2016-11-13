e30dd7788547c1e0762606bfa715934e60231e7e
Brad Bishop <bradleyb@fuzziesquirrel.com>
2016-11-12 21:39:28 -0500

    build: Fix out of tree build issues
    Eventually server.hpp will come from an external shared
    library.  Until then, iquote it while it lives in-tree.
    
    Change-Id: If0f7da8e2c951443fea0d623ad03a7c54a5a5ee9
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- test/test.cpp
+++ test/test.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "manager.hpp"
+#include "../manager.hpp"
 #include "../config.h"
 #include <cassert>
 
