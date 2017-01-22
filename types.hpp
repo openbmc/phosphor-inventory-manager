c9cc68f1b2c8bd0656c655edcda0903e14e48c69
Brad Bishop <bradleyb@fuzziesquirrel.com>
2017-01-22 01:03:02 -0500

    Remove Object from testcase
    Currently the testcase and the application both define their
    own Object type.  Move to a common header.
    
    Change-Id: I05dba67dca7855f522be299fcfd361913ed73fbc
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

---
+++ types.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <sdbusplus/message.hpp>
+
+namespace phosphor
+{
+namespace inventory
+{
+namespace manager
+{
+
+/** @brief Inventory manager supported property types. */
+using InterfaceVariantType = sdbusplus::message::variant<int64_t, std::string>;
+
+template <typename T>
+using InterfaceType = std::map<std::string, T>;
+
+template <typename T>
+using ObjectType = std::map<std::string, InterfaceType<T>>;
+
+using Interface = InterfaceType<InterfaceVariantType>;
+using Object = ObjectType<InterfaceVariantType>;
+
+} // namespace manager
+} // namespace inventory
+} // namespace phosphor
+
+// vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
