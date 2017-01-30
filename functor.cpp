686e5181b8d8ada8332f077d1a415bb2728e2418
Brad Bishop <bradleyb@fuzziesquirrel.com>
2017-01-30 08:52:26 -0500

    Add path conditions
    Add support to setProperty and destroyObject to conditionally
    perform their action based on the result of a condition testing
    functor.
    
    Change-Id: I67ded31f4a7ee0f7a29bb6edc06ebf9249cdc070
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- functor.cpp
+++ functor.cpp
@@ -26,10 +26,18 @@ namespace functor
 {
 namespace details
 {
-
 bool PropertyConditionBase::operator()(
     sdbusplus::bus::bus& bus,
     sdbusplus::message::message&,
+    Manager& mgr) const
+{
+    std::string path(_path);
+    return (*this)(path, bus, mgr);
+}
+
+bool PropertyConditionBase::operator()(
+    const std::string& path,
+    sdbusplus::bus::bus& bus,
     Manager&) const
 {
     std::string host;
@@ -45,7 +53,7 @@ bool PropertyConditionBase::operator()(
                               "/xyz/openbmc_project/ObjectMapper",
                               "xyz.openbmc_project.ObjectMapper",
                               "GetObject");
-        mapperCall.append(_path);
+        mapperCall.append(path);
         mapperCall.append(std::vector<std::string>({_iface}));
 
         auto mapperResponseMsg = bus.call(mapperCall);
@@ -72,7 +80,7 @@ bool PropertyConditionBase::operator()(
     }
     auto hostCall = bus.new_method_call(
                         host.c_str(),
-                        _path.c_str(),
+                        path.c_str(),
                         "org.freedesktop.DBus.Properties",
                         "Get");
     hostCall.append(_iface);
