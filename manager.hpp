dbf0aefd43a8cf3d334aa860aa0986c30aeae1e3
Brad Bishop <bradleyb@fuzziesquirrel.com>
2017-02-08 23:34:59 -0500

    Use std::function
    Replace CallableHolder with std::function.  No need to re-invent
    the wheel.
    
    Change-Id: I2647a802237dba4a48187718f0d3da59e97575d7
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- manager.hpp
+++ manager.hpp
@@ -81,7 +81,7 @@ class Manager final :
 
         using EventInfo = std::tuple <
                           std::vector<details::EventBasePtr>,
-                          std::vector<details::ActionBasePtr >>;
+                          std::vector<details::Action >>;
 
         /** @brief Start processing DBus messages. */
         void run() noexcept;
