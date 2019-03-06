4a1a2d6d4f581ce818a0bc5d6b0d12299da88867
Matt Spinler <spinler@us.ibm.com>
2019-03-06 14:11:22 -0600

    Load the associations definitions
    Parse the JSON file to load in the _associations
    data structures.
    
    The JSON looks like:
    
    [
        {
            "path": "The relative path of the inventory object to create the
                     org.openbmc.Associations interface on."
            "endpoints":
            [
                {
                    "types":
                    {
                        "fType": "The forward association type."
                        "rType": "The reverse association type."
                    },
                    "paths":
                    [
                        "The list of association endpoints for this
                         inventory path and association type."
                    ]
                }
            ]
        }
    ]
    
    Change-Id: I098fdc607f0c3ab2861f9b33e3e0d46e4989bd7a
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- association_manager.cpp
+++ association_manager.cpp
@@ -2,6 +2,11 @@
 
 #include "association_manager.hpp"
 
+#include <experimental/filesystem>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/log.hpp>
+
 namespace phosphor
 {
 namespace inventory
@@ -10,10 +15,66 @@ namespace manager
 {
 namespace associations
 {
+using namespace phosphor::logging;
+using sdbusplus::exception::SdBusError;
 
 Manager::Manager(sdbusplus::bus::bus& bus, const std::string& jsonPath) :
     _bus(bus), jsonFile(jsonPath)
 {
+    load();
+}
+
+void Manager::load()
+{
+    try
+    {
+        // Load the contents of jsonFile into _associations.
+
+        if (!std::experimental::filesystem::exists(jsonFile))
+        {
+            using namespace phosphor::logging;
+            log<level::ERR>("Could not find associations file",
+                            entry("FILE=%s", jsonFile.c_str()));
+            return;
+        }
+
+        std::ifstream file{jsonFile};
+
+        auto json = nlohmann::json::parse(file, nullptr, true);
+
+        const std::string root{INVENTORY_ROOT};
+
+        for (const auto& jsonAssoc : json)
+        {
+            // Only add the slash if necessary
+            std::string path = jsonAssoc["path"];
+            if (path.front() != '/')
+            {
+                path = root + "/" + path;
+            }
+            else
+            {
+                path = root + path;
+            }
+
+            auto& assocEndpoints = _associations[path];
+
+            for (const auto& endpoint : jsonAssoc["endpoints"])
+            {
+                Types types{endpoint["types"]["fType"],
+                            endpoint["types"]["rType"]};
+
+                Paths paths = endpoint["paths"];
+                assocEndpoints.emplace_back(std::move(types), std::move(paths));
+            }
+        }
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>(
+            "Failed loading associations json file. Not creating associations",
+            entry("FILE=%s", jsonFile.c_str()), entry("ERROR=%s", e.what()));
+    }
 }
 
 void Manager::createAssociations(const std::string& objectPath)
