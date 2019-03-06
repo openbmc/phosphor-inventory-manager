549ea45d11d0e5eba3110f422343000c639777b8
Matt Spinler <spinler@us.ibm.com>
2019-03-06 14:37:42 -0600

    Create associations
    When a new inventory object is created, check if it has an association
    that needs to be created in the data loaded from the JSON associations
    configuration file.  If it does, then check if the association endpoint
    D-Bus path exists using a GetObject D-Bus call.
    
    If the endpoint path exists, then create an org.openbmc.Associations
    interface object for the association.
    
    If it doesn't, then create an interfacesAdded watch on that path, and
    then when it does show up at that time create the association interface.
    
    When openbmc/phosphor-objmgr#15 is done, the mapper will handle having
    the lifetime of the association objects it creates match the lifetime of
    both endpoints.  That means it will handle removing the association
    paths when an endpoint disappears, and while it would also handle not
    creating the association paths until both endpoints are there, this code
    is still doing the interfacesAdded check so it doesn't bother the mapper
    with something that may never show up, not to mention cluttering the
    inventory interfaces with association interfaces that may never get
    used.
    
    Change-Id: Ie850601cbd0acba631c5b66cb4541decf66e286d
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- association_manager.cpp
+++ association_manager.cpp
@@ -40,17 +40,17 @@ void Manager::load()
 {
     try
     {
-        // Load the contents of jsonFile into _associations.
+        // Load the contents of _jsonFile into _associations.
 
-        if (!std::filesystem::exists(jsonFile))
+        if (!std::filesystem::exists(_jsonFile))
         {
             using namespace phosphor::logging;
             log<level::ERR>("Could not find associations file",
-                            entry("FILE=%s", jsonFile.c_str()));
+                            entry("FILE=%s", _jsonFile.c_str()));
             return;
         }
 
-        std::ifstream file{jsonFile};
+        std::ifstream file{_jsonFile};
 
         auto json = nlohmann::json::parse(file, nullptr, true);
 
@@ -90,15 +90,149 @@ void Manager::load()
     {
         log<level::ERR>(
             "Failed loading associations json file. Not creating associations",
-            entry("FILE=%s", jsonFile.c_str()), entry("ERROR=%s", e.what()));
+            entry("FILE=%s", _jsonFile.c_str()), entry("ERROR=%s", e.what()));
         _associations.clear();
     }
 }
 
+bool Manager::exists(const std::string& objectPath)
+{
+    try
+    {
+        auto method = _bus.new_method_call("xyz.openbmc_project.ObjectMapper",
+                                           "/xyz/openbmc_project/object_mapper",
+                                           "xyz.openbmc_project.ObjectMapper",
+                                           "GetObject");
+        method.append(objectPath, std::vector<std::string>{});
+        auto response = _bus.call(method);
+
+        std::map<std::string, std::vector<std::string>> object;
+        response.read(object);
+
+        if (!object.empty())
+        {
+            return true;
+        }
+    }
+    catch (SdBusError& e)
+    {
+    }
+
+    return false;
+}
+
 void Manager::createAssociations(const std::string& objectPath)
 {
-    // TODO
+    auto endpoints = _associations.find(objectPath);
+    if (endpoints == _associations.end())
+    {
+        return;
+    }
+
+    if (std::find(_handled.begin(), _handled.end(), objectPath) !=
+        _handled.end())
+    {
+        return;
+    }
+
+    _handled.push_back(objectPath);
+
+    for (const auto& endpoint : endpoints->second)
+    {
+        const auto& types = std::get<typesPos>(endpoint);
+        const auto& paths = std::get<pathsPos>(endpoint);
+
+        for (const auto& endpointPath : paths)
+        {
+            if (exists(endpointPath))
+            {
+                const auto& forwardType = std::get<forwardTypePos>(types);
+                const auto& reverseType = std::get<reverseTypePos>(types);
+
+                createAssociation(objectPath, forwardType, endpointPath,
+                                  reverseType);
+            }
+            else
+            {
+                ifacesAddedWatch(endpointPath);
+            }
+        }
+    }
 }
+
+void Manager::createAssociation(const std::string& forwardPath,
+                                const std::string& forwardType,
+                                const std::string& reversePath,
+                                const std::string& reverseType)
+{
+    auto object = _associationIfaces.find(forwardPath);
+    if (object == _associationIfaces.end())
+    {
+        auto a = std::make_unique<AssociationObject>(_bus, forwardPath.c_str(),
+                                                     true);
+
+        using AssociationProperty =
+            std::vector<std::tuple<std::string, std::string, std::string>>;
+        AssociationProperty prop;
+
+        prop.emplace_back(forwardType, reverseType, reversePath);
+        a->associations(std::move(prop));
+        a->emit_object_added();
+        _associationIfaces.emplace(forwardPath, std::move(a));
+    }
+    else
+    {
+        // Interface exists, just update the property
+        auto prop = object->second->associations();
+        prop.emplace_back(forwardType, reverseType, reversePath);
+        object->second->associations(std::move(prop));
+    }
+}
+
+void Manager::ifacesAddedWatch(const std::string& objectPath)
+{
+    if (_addMatches.find(objectPath) != _addMatches.end())
+    {
+        return;
+    }
+
+    auto match = std::make_unique<sdbusplus::bus::match_t>(
+        _bus,
+        sdbusplus::bus::match::rules::interfacesAdded() +
+            sdbusplus::bus::match::rules::argNpath(0, objectPath),
+        std::bind(std::mem_fn(&Manager::interfaceAdded), this,
+                  std::placeholders::_1));
+
+    _addMatches.emplace(objectPath, std::move(match));
+}
+
+void Manager::interfaceAdded(sdbusplus::message::message& msg)
+{
+    sdbusplus::message::object_path objectPath;
+    msg.read(objectPath);
+
+    // The path that was just added will be in an endpoints list.
+    // Find it and create the association object.
+
+    for (const auto& [invPath, endpoints] : _associations)
+    {
+        for (const auto& endpoint : endpoints)
+        {
+            const auto& paths = std::get<pathsPos>(endpoint);
+            auto path = std::find(paths.begin(), paths.end(), objectPath.str);
+
+            if (path != paths.end())
+            {
+                const auto& types = std::get<typesPos>(endpoint);
+                createAssociation(invPath, std::get<forwardTypePos>(types),
+                                  *path, std::get<reverseTypePos>(types));
+            }
+        }
+    }
+
+    _addMatches.erase(objectPath);
+}
+
 } // namespace associations
 } // namespace manager
 } // namespace inventory
