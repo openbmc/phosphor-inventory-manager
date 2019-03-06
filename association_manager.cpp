838b8d15707d38a9435200ac3988c77a73272d5e
Matt Spinler <spinler@us.ibm.com>
2019-03-06 13:46:14 -0600

    Add associations::Manager class
    This class will handle creating the org.openbmc.Associations
    D-Bus interfaces on inventory paths if it has been told to
    by a JSON configuration file.
    
    Every time PIM creates a new inventory path, this class will
    check to see if any associations need to be created.
    
    This functionality is optional, and is off by default.  To
    enable it, the --enable-associations configure option needs to
    be used.
    
    This commit just introduces the class.
    
    Change-Id: I70045b768f3db683bcfac0c7d4bc9798eb10a5c5
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ association_manager.cpp
@@ -0,0 +1,24 @@
+#include "association_manager.hpp"
+
+namespace phosphor
+{
+namespace inventory
+{
+namespace manager
+{
+namespace associations
+{
+
+Manager::Manager(sdbusplus::bus::bus& bus, const std::string& jsonPath) :
+    _bus(bus), _jsonFile(jsonPath)
+{
+}
+
+void Manager::createAssociations(const std::string& objectPath)
+{
+    // TODO
+}
+} // namespace associations
+} // namespace manager
+} // namespace inventory
+} // namespace phosphor
