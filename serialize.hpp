openbmc/phosphor-inventory-manager

commit e6b21c74581c30ea635aabbbc08d0a56f8e27063

cleanup: Use filesystem instead of experimental
With Thud(Yocto 2.6) bumping gcc version, std::filesystem
is available.

Change-Id: I1837df14e52be3e2007b941c3f9c0d3efc687027
Signed-off-by: Kun Yi <kunyi731@gmail.com>

serialize.hpp
@@ -3,7 +3,7 @@
 #include "config.h"
 
 #include <cereal/archives/json.hpp>
-#include <experimental/filesystem>
+#include <filesystem>
 #include <fstream>
 #include <phosphor-logging/log.hpp>
 
@@ -14,7 +14,7 @@ namespace inventory
 namespace manager
 {
 
-namespace fs = std::experimental::filesystem;
+namespace fs = std::filesystem;
 
 struct SerialOps
 {

manager.cpp
@@ -20,7 +20,7 @@
 #include <algorithm>
 #include <chrono>
 #include <exception>
-#include <experimental/filesystem>
+#include <filesystem>
 #include <iostream>
 #include <phosphor-logging/log.hpp>
 
@@ -307,7 +307,7 @@ const std::any& Manager::getInterfaceHolder(const char* path,
 
 void Manager::restore()
 {
-    namespace fs = std::experimental::filesystem;
+    namespace fs = std::filesystem;
 
     if (!fs::exists(fs::path(PIM_PERSIST_PATH)))
     {

commit a24855045867171309a54f0de6ac99761654aa34

serialization: adapt to filesystem API changes
The behavior of std::filesystem::path::append changed on its journey
from experimental:: to std::
Add a testcase and adapt to the behavior change.

Fixes: e6b21c74581c30ea635aabbbc08d0a56f8e27063
Change-Id: I4bdb43ea9b85933ec12ae0f4b609ec3a33d77d01
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

serialize.hpp
@@ -16,6 +16,18 @@ namespace manager
 
 namespace fs = std::filesystem;
 
+namespace detail
+{
+inline fs::path getStoragePath(const std::string& path,
+                               const std::string& iface)
+{
+    auto p = fs::path(PIM_PERSIST_PATH);
+    p /= fs::path(path).relative_path();
+    p /= fs::path(iface).relative_path();
+    return p;
+}
+} // namespace detail
+
 struct SerialOps
 {
     /** @brief Serialize inventory item path
@@ -26,10 +38,8 @@ struct SerialOps
      */
     static void serialize(const std::string& path, const std::string& iface)
     {
-        fs::path p(PIM_PERSIST_PATH);
-        p /= path;
-        fs::create_directories(p);
-        p /= iface;
+        auto p = detail::getStoragePath(path, iface);
+        fs::create_directories(p.parent_path());
         std::ofstream os(p, std::ios::binary);
     }
 
@@ -43,10 +53,8 @@ struct SerialOps
     static void serialize(const std::string& path, const std::string& iface,
                           const T& object)
     {
-        fs::path p(PIM_PERSIST_PATH);
-        p /= path;
-        fs::create_directories(p);
-        p /= iface;
+        auto p = detail::getStoragePath(path, iface);
+        fs::create_directories(p.parent_path());
         std::ofstream os(p, std::ios::binary);
         cereal::JSONOutputArchive oarchive(os);
         oarchive(object);

test/Makefile.am
@@ -23,7 +23,7 @@ check_PROGRAMS += utils-test
 
 serialize_test_SOURCES = serialize_test.cpp
 serialize_test_CFLAGS = ${GTEST_CFLAGS} ${GMOCK_CFLAGS}
-serialize_test_LDADD = ${GTEST_LIBS} ${GMOCK_LIBS}
+serialize_test_LDADD = ${GTEST_LIBS} ${GMOCK_LIBS} -lstdc++fs
 serialize_test_LDFLAGS = ${OESDK_TESTCASE_FLAGS}
 check_PROGRAMS += serialize-test
 

test/serialize_test.cpp
@@ -1,1 +1,24 @@
 #include "../serialize.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace phosphor::inventory::manager;
+using namespace std::string_literals;
+
+TEST(SerializeTest, TestStoragePathNoSlashes)
+{
+    auto path = "foo/bar/baz"s;
+    auto iface = "xyz.foo"s;
+    auto p1 = detail::getStoragePath(path, iface);
+    auto p2 = fs::path(PIM_PERSIST_PATH "/foo/bar/baz/xyz.foo");
+    EXPECT_EQ(p1, p2);
+}
+
+TEST(SerializeTest, TestStoragePathSlashes)
+{
+    auto path = "/foo/bar/baz"s;
+    auto iface = "/xyz.foo"s;
+    auto p1 = detail::getStoragePath(path, iface);
+    auto p2 = fs::path(PIM_PERSIST_PATH "/foo/bar/baz/xyz.foo");
+    EXPECT_EQ(p1, p2);
+}