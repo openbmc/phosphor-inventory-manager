openbmc/phosphor-inventory-manager

commit 23a8d935b326eedc7ae1ff826e6994a0fba0b341

don't emit unnecessary signals
Fix a number of places where DBus signals are emitted when they should
not be:

-Prior to claiming a well known DBus service name
-When using the map-of-properties sdbusplus interface constructor

On a system with ~175 inventory interfaces this eliminates 650
PropertiesChanged and ObjectManager signals on PIM startup.

Change-Id: I7b8fa6af69ca3553f5b2ea49e488a75a592a4d0b
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

manager.cpp
@@ -167,17 +167,21 @@ void Manager::updateInterfaces(const sdbusplus::message::object_path& path,
             {
                 // Add the new interface.
                 auto& ctor = std::get<MakeInterfaceType>(opsit->second);
+                // skipSignal = true here to avoid getting PropertiesChanged
+                // signals while the interface is constructed.  We'll emit an
+                // ObjectManager signal for this interface below.
                 refaceit = refaces.insert(
                     refaceit, std::make_pair(ifaceit->first,
                                              ctor(_bus, path.str.c_str(),
-                                                  ifaceit->second, false)));
+                                                  ifaceit->second, true)));
                 signals.push_back(ifaceit->first);
             }
             else
             {
                 // Set the new property values.
                 auto& assign = std::get<AssignInterfaceType>(opsit->second);
-                assign(ifaceit->second, refaceit->second, false);
+                assign(ifaceit->second, refaceit->second,
+                       _status != ManagerStatus::RUNNING);
             }
             if (!restoreFromCache)
             {
@@ -204,13 +208,16 @@ void Manager::updateInterfaces(const sdbusplus::message::object_path& path,
         ++ifaceit;
     }
 
-    if (newObject)
+    if (_status == ManagerStatus::RUNNING)
     {
-        _bus.emit_object_added(path.str.c_str());
-    }
-    else if (!signals.empty())
-    {
-        _bus.emit_interfaces_added(path.str.c_str(), signals);
+        if (newObject)
+        {
+            _bus.emit_object_added(path.str.c_str());
+        }
+        else if (!signals.empty())
+        {
+            _bus.emit_interfaces_added(path.str.c_str(), signals);
+        }
     }
 }
 
@@ -245,7 +252,8 @@ void Manager::updateObjects(
 #ifdef CREATE_ASSOCIATIONS
         if (newObj)
         {
-            _associations.createAssociations(absPath, false);
+            _associations.createAssociations(absPath,
+                                             state != ManagerStatus::RUNNING);
         }
 #endif
         ++objit;

gen_serialization.mako.hpp
@@ -16,6 +16,10 @@
 CEREAL_CLASS_VERSION(${iface.namespace()}, CLASS_VERSION);
 % endfor
 
+// Emitting signals prior to claiming a well known DBus service name causes
+// un-necessary DBus traffic and wakeups.  De-serialization only happens prior
+// to claiming a well known name, so don't emit signals.
+static constexpr auto skipSignals = true;
 namespace cereal
 {
 
@@ -48,7 +52,7 @@ void load(Archive& a,
 %>\
     a(${props});
 % for p in properties:
-<% t = "object." + p.camelCase + "(" + p.CamelCase + ")" %>\
+<% t = "object." + p.camelCase + "(" + p.CamelCase + ", skipSignals)" %>\
     ${t};
 % endfor
 }

commit fe409346819f59681bf8489d9ce7c0b6141e76e1

manager: fix build fail with CREATE_ASSOCIATIONS
Manager doesn't have any "state" member.  The correct member is
_status"state" member.  The correct member is _status.

Fixes: 23a8d935b326eedc7ae1ff826e6994a0fba0b341
Change-Id: I30497ef9b2dd1c9f8cf19a50328d5717eb0611ad
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

manager.cpp
@@ -253,7 +253,7 @@ void Manager::updateObjects(
         if (newObj)
         {
             _associations.createAssociations(absPath,
-                                             state != ManagerStatus::RUNNING);
+                                             _status != ManagerStatus::RUNNING);
         }
 #endif
         ++objit;